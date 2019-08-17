72272aee28fb17f4c10845b512a27d983be8074f
STIC <stic@Predator-2.60>
2019-08-17 09:39:00 -0400

    change app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Bye there you old fart!
+            Bye there you old fart Spud!
         </p>
         <a
           className="App-link"
